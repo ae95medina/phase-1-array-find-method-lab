310e685a29afde0f760b05f981e29122e247b283
ae95medina <ae95medina@gmail.com>
2022-05-11 19:39:43 -0500

    commit tree e217e0618d461b0e4436518482af06c430f8a931

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// code your solution here
+function superbowlWin(record){    
+let win = record.find(object => object.result === 'W')
+    if(win){
+        return win.year
+    }
+    else {
+    return undefined
+    }
+}
